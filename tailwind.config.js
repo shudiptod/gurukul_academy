3f1c1bbc6d2b4adc9efb9f951cb5e505eabb6d1d
shudipto <shudipta15-11424@diu.edu.bd>
2021-10-05 04:53:27 +0600

    customized viewpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                'sm': { 'min': '360px', 'max': '412px' },
+                'md': { 'min': '600px', 'max': '810px' },
+                'lg': { 'min': '811px', 'max': '1368px' }
+            }
+        },
     },
     variants: {
         extend: {},
